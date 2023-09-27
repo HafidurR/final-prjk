eec52ab3ec840925855cec73c940ff58da6e432d
HafidurR <deifah126@gmail.com>
2023-09-27 13:58:55 +0700

    prjk init

---
+++ src/model/migration.go
@@ -0,0 +1,20 @@
+package model
+
+import "gorm.io/gorm"
+
+type Author struct {
+	gorm.Model
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string    `json:"password"`
+	Age      int    `json:"age"`
+	Books 		[]Book
+}
+
+type Book struct {
+	gorm.Model
+	Title  				string `json:"title"`
+	Description   string `json:"description"`
+	Publisher 		string	`json:"publisher`
+	AuthorID 			int			`json:"author_id"`
+}
