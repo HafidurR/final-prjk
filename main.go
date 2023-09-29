99c94b94df9d38773cb8dbb1f5c5e5362d3a6307
HafidurR <deifah126@gmail.com>
2023-09-29 08:39:54 +0700

    change file config & main

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/HafidurR/final-prjk/src/config"
+	"github.com/HafidurR/final-prjk/src/routes"
+	"github.com/joho/godotenv"
+)
+
+
+func main()  {
+	loadEnv()
+	fmt.Println("ya error", os.Getenv("DB_NAME"))	
+	config.Connect()
+	routes.HandleRequest()
+}
+
+func loadEnv(){
+	err := godotenv.Load()
+  	if err != nil {
+    	panic("Error loading .env file")
+  	}
+}
--- src/config/connection.go
+++ src/config/connection.go
@@ -1,1 +1,36 @@
 package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/HafidurR/final-prjk/src/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+var err error
+
+func Connect() *gorm.DB {
+	// db, err = gorm.Open(mysql.Open("root:@tcp(localhost:3306)/db_final_prjk?parseTime=true"))
+
+	dsn := fmt.Sprintf("%v:@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local",
+				os.Getenv("DB_USERNAME"),
+				// os.Getenv("DB_PASSWORD"),
+				os.Getenv("DB_HOST"),
+				os.Getenv("DB_PORT"),
+				os.Getenv("DB_NAME"))
+	
+	fmt.Println("ya error cok", os.Getenv("DB_NAME"))	
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Println("connection failed", err)
+	} else {
+		log.Println("connection succeeded")
+	}
+
+	db.AutoMigrate(&models.Author{}, &models.Book{})
+	return db
+}
